ab8391ee1e5fee988f515cd1782fefaccca1e30e
coheleth <diogopiccirillo@gmail.com>
2023-01-04 17:09:03 -0300

    Fixed long_description

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import find_packages, setup
+README = open('README.md').read()
+
 setup(
     name='emailbuilder',
     packages=find_packages(exclude=['test']),
     python_requires='>3.7',
     version='0.1.0',
     description='Simple HTML e-mail generator',
+    long_description_content_type="text/markdown",
+    long_description=README,
     author='Coheleth',
     license='MIT',
+    url='https://github.com/coheleth/emailbuilder',
     install_requires=[],
     setup_requires=['pytest-runner'],
     tests_require=['pytest==4.4.1'],
-    test_suite='tests'
+    # test_suite='tests'
 )
